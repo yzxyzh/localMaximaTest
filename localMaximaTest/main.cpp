ad072c9e731fa0bcb35471ad2fc5488280d2e411
Yanzixu <yanzixu@YanzixudeMacBook-Pro.local>
2014-08-26 18:29:51 +0800

    Initial Commit

---
+++ localMaximaTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  localMaximaTest
+//
+//  Created by Yanzixu on 14-8-26.
+//  Copyright (c) 2014年 Yanzixu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
